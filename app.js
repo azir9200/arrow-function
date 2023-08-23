d4050a7c8936c92c4d18391e05e19e9896c01aae
Azir@9200 <aziruddin83@hotmail.com>
2023-08-23 13:13:29 +0100

    created files

---
+++ app.js
@@ -0,0 +1,18 @@
+const game = {
+  title: "footbal",
+  player: "ronaldo",
+  origin: "portugal",
+  age: 35,
+  money: 203435,
+  match: {
+    barcelona: 1999,
+    with: "messy",
+    another: "neymar",
+  },
+};
+let { title, player, age, match } = game;
+//console.log(title, player, age, match);
+const book = ['boser Bashi', 'gitanjoli', ' poter Kta'];
+let [nazrul, robin, robin] = book;
+console.log()
+
---
+++ solvone.js
@@ -0,0 +1,124 @@
+// const one = 5;
+// const two = 8;
+// const three = 9;
+// const mult = (one * two * three);
+// //console.log(mult);
+// const ten = 876;
+// const nine = 345;
+// const left = (ten / nine);
+// //console.log(left);
+// const azir = (jas, riza, ara) => jas * riza * ara;
+// //const  jas, riza, ara = 
+// //console.log(2, 3, 4);
+
+// let couunt = 5;
+// let count = couunt + 5;
+
+// const nums = [23, 34, 35, 24, 54, 76, 68]
+// nums.pop();
+// nums[3] = 50;
+// nums.push(102, 204, 234, 350); v
+// //console.log(nums);
+
+
+// const name = 'azir uddin';
+// for (let char of name) {
+//   //console.log(char);
+// }
+// const money = 23;
+// const taka = money + 24;
+// const euro = taka + 100;
+//console.log(euro);
+// 
+
+// let sum = 0;
+// for (let i = 1; i < 15; i + ) {
+//   sum = sum + 2;
+//   console.log(sum);
+// }
+
+// function add(num1, num2 = 0) {
+//   const result = num1 + num2;
+//   cobsole.log(num1, num2, result);
+//   return result;
+
+// }
+//const sum = add(45);
+
+// const a = 10;
+// const b = 12;
+// const c = 13;
+// const sum = `the sum of ${a}, ${b}, ${c} is ${a + b + c} `;
+// console.log(sum);
+// let azir = 40;
+// let jasima = 35;
+// let araf = 3;
+// let total = ` total is ${azir + jasima + araf};`
+// // console.log(total);
+// const amir = 940;
+// const expnse = 920;
+// const inhand = `${amir - expnse};`
+// console.log(inhand);
+// const you = 24;
+// const me = 34;
+// const mult = `${you * me}`;
+// console.log(mult);
+// const email = `hi john`
+//   `tor ki ase mon`
+//   `ki chas ekon`
+//   ;
+// console.log(email);
+
+// const nums = [34, 45, 54, 37, 58];
+// const student = {
+//   name: 'araf',
+//   age: 52,
+//   conty: 'port'
+// };
+// //console.log(student);
+// const math = `add ${nums} and ${student.age} is ${nums[2] + nums[3] + nums[4] + student.age}`;
+// console.log(math);
+// function add(a, b) {
+//   const result = a + b;
+//   return result;
+// }
+// const sum = add(45, 46);
+// console.log(sum);
+
+// function add(a, b) {
+//   return a + b;
+// }
+// let susm = add(23, 12);
+// console.log(sum);
+
+// function add(a, b) {
+//   return a + b;
+// }
+// let raha = add(54, 56);
+// console.log(raha);
+
+// const add = function (a, b) {
+//   return a + b;
+// }
+// const ruhi = add(60, 60);
+// console.log(ruhi);
+// const add = (b, c, d) => b * c * d;
+// const roni = add(93, 5, 5)
+// // console.log(roni);
+// const myName = () =>
+//   console.log('sam');
+// let added = () => 'joni'
+// console.log(added);
+// const guun = (a, b) => a * b;
+// const agun = guun(2, 6);
+// console.log(agun);
+
+const getAge = (person) => person.age;
+const person = {
+  name: 'porto',
+  place: 'europ',
+  age: 24,
+};
+const age = getAge(person);
+console.log(age);
+
